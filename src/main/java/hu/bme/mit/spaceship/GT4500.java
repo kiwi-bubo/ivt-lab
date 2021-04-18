4c8c4e16bff65e61bced3ac2ace1a62b75ff69b1
kiwi-bubo <b.kingaviola@gmail.com>
2021-04-18 20:33:10 +0200

    Fix torpedo

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,10 +78,11 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
-        // try to fire both of the torpedo stores
-        firingSuccess = primaryTorpedoStore.fire(1);
-        firingSuccess = secondaryTorpedoStore.fire(1);
-
+        if(!primaryTorpedoStore.isEmpty() && ! secondaryTorpedoStore.isEmpty())
+          // try to fire both of the torpedo stores
+          firingSuccess = primaryTorpedoStore.fire(1);
+          firingSuccess = secondaryTorpedoStore.fire(1);
+          wasPrimaryFiredLast = false;
         break;
     }
 
